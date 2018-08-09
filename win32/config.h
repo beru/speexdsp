042360099f4f3540d1c02f828a57e8b88067465f
Katsuhisa Yuasa <berupon@gmail.com>
2018-08-09 10:24:59 +0900

    VS2017用のファイルを追加

--- win32/config.h
+++ win32/config.h
@@ -5,11 +5,11 @@
 // _M_IX86_FP=2 means /arch:SSE2 was used.
 // Also, enable both _USE_SSE and _USE_SSE2 if we're compiling for x86-64
 #if _M_IX86_FP >= 1 || defined(_M_X64)
-#define _USE_SSE
+//#define _USE_SSE
 #endif
 
 #if _M_IX86_FP >= 2 || defined(_M_X64)
-#define _USE_SSE2
+//#define _USE_SSE2
 #endif
 
 // Visual Studio support alloca(), but it always align variables to 16-bit
